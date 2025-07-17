fa852e25f8076315e8841df8086cbbf56aa9a653
Carl Brugger <carl.brugger@flatfile.io>
2025-07-16 19:09:24 -0500

    remove debug listener

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,4 @@ export default function (listener: FlatfileListener) {
   handlers.forEach((handler) => listener.use(handler))
 
   listener.use(ExcelExtractor())
-
-  // Disabled for deployed agents
-  if (!process.env.LAMBDA_TASK_ROOT) {
-    listener.on('**', (event) => {
-      console.log(`Received event: ${event.topic}`)
-    })
-  }
 }
